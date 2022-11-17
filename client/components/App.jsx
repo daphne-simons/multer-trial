79ecbd5d59d62c958080031e967b7588a1fc09b4
Daphne Simons <daphnejasminesimons@gmail.com>
2022-11-17 21:32:55 +1300

    backend works

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,34 +1,40 @@
-import React, { useState, useEffect } from 'react'
-import { getGreeting } from '../apiClient'
+import React, { useState } from 'react'
 
 const App = () => {
-  const [greeting, setGreeting] = useState('')
-  const [count, setCount] = useState(0)
-  const [isError, setIsError] = useState(false)
+  const [image, setImage] = useState({})
+  const fileOnChange = (event) => {
+    console.log(event.target.files[0])
+  }
 
-  useEffect(() => {
-    getGreeting()
-      .then((greeting) => {
-        console.log(greeting)
-        setGreeting(greeting)
-        setIsError(false)
-      })
-      .catch((err) => {
-        console.log(err)
-        setIsError(true)
-      })
-  }, [count])
+  const sendImage = (event) => {
+    const formData = new formData()
 
+    formData.append('avatar', image)
+
+    // fetch('http://localhost:3000/uploadFile', {
+    //   method: 'post',
+    //   body: formData,
+    // })
+    //   .then((res) => res.text())
+    //   .then((resBody) => {
+    //     console.log(resBody)
+    //   })
+    //   .catch((err) => {
+    //     console.error(err.message)
+    //   })
+  }
   return (
     <>
-      {count}
-      <h1>{greeting}</h1>
-      {isError && (
-        <p style={{ color: 'red' }}>
-          There was an error retrieving the greeting.
-        </p>
-      )}
-      <button onClick={() => setCount(count + 1)}>Click</button>
+      <h1> Add an Image!</h1>
+
+      <div>
+        <form method="POST" action="/uploadFile" encType="multipart/form-data">
+          <input type="file" name="avatar" onChange={fileOnChange} />
+        </form>
+        <button onClick={sendImage}> Upload me!</button>
+      </div>
+
+      <h2>Looking good!</h2>
     </>
   )
 }
--- server/server.js
+++ server/server.js
@@ -1,18 +1,33 @@
 const path = require('path')
 const express = require('express')
+// const app = express()
 const cors = require('cors')
+const fs = require('fs')
+// multer must haves:
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 const server = express()
 
 server.use(express.json())
-server.use(express.static(path.join(__dirname, './public')))
+server.use(express.static(path.join(__dirname, './uploads')))
 server.use(cors('*'))
 
-server.get('/greeting', (req, res) => {
-  const greetings = ['hola', 'hi', 'hello', 'howdy']
-  let index = Math.floor(Math.random() * greetings.length)
-  console.log(index)
-  res.json({ greeting: greetings[index] })
+//multer logic for uploading images
+server.post('/uploadFile', upload.single('avatar'), function (req, res) {
+  const fileType = req.file.mimetype.split('/')[1]
+  console.log(fileType)
+  const newFileName = req.file.filename + '.' + fileType
+  fs.rename(
+    `./uploads/${req.file.filename}`,
+    `./uploads/${newFileName}`,
+    function () {
+      console.log('callback')
+    }
+  )
+  res.sendStatus(200)
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
 })
 
 module.exports = server
